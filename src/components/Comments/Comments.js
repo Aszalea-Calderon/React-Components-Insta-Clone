4399062bbfb87113bf81060c83b730398d3aa196
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-12-08 13:30:18 -0800

    fixed comments, added post, post

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import Comment from './Comment';
-import './Comments.css';
+import React from "react";
 
-const Comments = props => {
+import Comment from "./Comment";
+import "./Comments.css";
+
+const Comments = (props) => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
+  //console.log(comments);
 
   return (
     <div>
+      {comments.map((comment) => {
+        return <Comment key={comments.id} comment={comment} />;
+      })}
       {/* map through the comments prop and render a Comment for every piece of data */}
     </div>
   );
